a00fcfe4d751cce13b631bbc1c77a5a8db0c7a8f
Ryan Moore <ryanmoore88@gmail.com>
2016-01-25 23:14:50 -0500

    Added answer view which shows the top solutions to the problem

--- game/urls.py
+++ game/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^(?P<num_hands>\d+)/(?P<seed>[\d\w]+)$', views.game, name='game'),
+    url(r'^answer/(?P<num_hands>\d+)/(?P<seed>[\d\w]+)$',
+        views.answer, name='answer'),
 ]
--- game/views.py
+++ game/views.py
@@ -3,15 +3,26 @@ from django.http import HttpResponse
 import random
 import string
 import itertools
+import re
 
 def game(request, num_hands, seed):
-    hands = list(gen_triplets_from_seed(seed, int(num_hands)))
-    return HttpResponse('Hands in play: {}'.format(hands))
+    hands = gen_triplets_from_seed(seed, int(num_hands))
+    text = ''
+    for hand in hands:
+        text += '{}<br>'.format(''.join(hand))
+    return HttpResponse('Hands in play:<br>{}'.format(text))
+
+def answer(request, num_hands, seed):
+    hands = gen_triplets_from_seed(seed, int(num_hands))
+    text = ''
+    for hand in hands:
+        text += '{}: {}<br>'.format(''.join(hand), find_shortest_words(hand, 10))
+    return HttpResponse('{}'.format(text))
 
 def gen_triplets_from_seed(seed, count):
     random.seed(seed)
     selected = random.sample(weighted_letter_set(), count*3)
-    return grouper(selected, 3)
+    return list(grouper(selected, 3))
 
 def weighted_letter_set():
     choices = string.ascii_uppercase
@@ -24,3 +35,11 @@ def grouper(iterable, n, fillvalue=None):
     # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
     args = [iter(iterable)] * n
     return itertools.zip_longest(fillvalue=fillvalue, *args)
+
+def find_shortest_words(hand, limit):
+    pattern = r'\b\w*{}\w*{}\w*{}\w*\s*$'.format(*''.join(hand).lower())
+    with open('/usr/share/dict/words', 'r') as infile:
+        matches = re.findall(pattern, infile.read(), flags=re.MULTILINE)
+    matches = [elt for elt in matches if len(elt) > 3 and elt.islower()]
+    matches.sort(key=lambda x: len(x))
+    return list(itertools.islice(matches, 0, limit))
